50238c128376d9502558e6c549ee104689c258de
Thiago Esteves <testeves@forte.io>
2023-03-24 15:56:17 -0300

    Added CI files to run software testing

--- config/runtime.exs
+++ config/runtime.exs
@@ -17,7 +17,7 @@ import Config
 # Alternatively, you can use `mix phx.gen.release` to generate a `bin/server`
 # script that automatically sets the env var above.
 # if System.get_env("PHX_SERVER") do
-  config :ex_snake, ExSnakeWeb.Endpoint, server: true
+config :ex_snake, ExSnakeWeb.Endpoint, server: true
 # end
 
 if config_env() == :prod do
--- test/ex_snake_web/controllers/page_controller_test.exs
+++ test/ex_snake_web/controllers/page_controller_test.exs
@@ -1,8 +1,13 @@
 defmodule ExSnakeWeb.PageControllerTest do
   use ExSnakeWeb.ConnCase
 
-  test "GET /", %{conn: conn} do
-    conn = get(conn, ~p"/")
-    assert html_response(conn, 200) =~ "Peace of mind from prototype to production"
+  test "GET /ready", %{conn: conn} do
+    conn = get(conn, ~p"/ready")
+    assert conn.resp_body =~ "OK"
+  end
+
+  test "GET /health", %{conn: conn} do
+    conn = get(conn, ~p"/health")
+    assert conn.resp_body =~ "OK"
   end
 end
